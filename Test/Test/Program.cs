1279c9d06f82432a531e478e4e27dc469f7eff82
Declan White <declan@gizagig.com>
2015-05-22 10:03:11 +0100

    TestCommit

---
+++ Test/Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            {
+                Console.WriteLine("Yes it is a leap year");
+            }
+            else
+            {
+                Console.WriteLine("No it is not leap year");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
